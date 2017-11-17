35807a59d9fb52ef2f1190b4468af668ae4458e7
Margaret Berry <codeberry1@gmail.com>
2017-11-17 14:32:22 -0800

    add rating range validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ActiveRecord::Base
   validates :author, :presence => true
   validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
   validates :rating, :presence => true
+  validates :rating, :inclusion => {:in => [1,2,3,4,5]}
 end
--- spec/features/add_review_pages_spec.rb
+++ spec/features/add_review_pages_spec.rb
@@ -18,4 +18,14 @@ describe "the add a review process" do
     click_on 'Create Review'
     expect(page).to have_content 'errors'
   end
+
+  it "gives error when rating is out of range" do
+    product = Product.create(:name => 'Lambas Bread', :cost => '10.00', :origin => 'Lorien')
+    visit new_product_review_path(product)
+    fill_in 'Author', :with => 'Frodo'
+    fill_in 'Content body', :with => 'This food is OK but could use a little more pizzazz'
+    fill_in 'Rating', :with => 7
+    click_on 'Create Review'
+    expect(page).to have_content 'errors'
+  end
 end
