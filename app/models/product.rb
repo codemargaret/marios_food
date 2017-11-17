00a2d51ca488f5739f77a72d37cf6135053e5c59
Margaret Berry <codeberry1@gmail.com>
2017-11-17 09:00:13 -0800

    set up database and create product and review classes

---
+++ app/models/product.rb
@@ -0,0 +1,7 @@
+class Product < ActiveRecord::Base
+  has_many :reviews
+
+  validates :name, :presence => true
+  validates :cost, :presence => true
+  validates :origin, :presence => true
+end
---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ActiveRecord::Base
+  belongs_to :product
+
+  validates :author, :presence => true
+  validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
+  validates :rating, :presence => true
+end
