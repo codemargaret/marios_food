bd870aa216c75c163bf8c518163779286b62b4f5
Margaret Berry <codeberry1@gmail.com>
2017-11-17 13:55:31 -0800

    add ability to write a new review for a product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,25 +1,25 @@
 class ReviewsController < ApplicationController
-  # def show
-  #   binding.pry
-  #   @product = Product.find(params[:id])
-  #
-  #   @reviews = @product.reviews
-  # end
+  def show
+    @review = Review.find(params[:id])
+    @product = @review.product
+  end
 
-  # def new
-  #   @product = Product.find(params[:product_id])
-  #   @review = @product.reviews.new
-  # end
-  #
-  # def create
-  #   @product = Product.find(params[:product_id])
-  #   @review = @product.reviews.new(review_params)
-  #   if @review.save
-  #     redirect_to product_path(@review.product)
-  #   else
-  #     render :new
-  #   end
-  # end
+  def new
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@review.product)
+    else
+      flash[:notice] = "Your product was not added."
+      render :new
+    end
+  end
 
 #   def edit
 #     @product = Product.find(params[:product_id])
@@ -43,8 +43,8 @@ class ReviewsController < ApplicationController
 #   end
 #
 #
-# private
-#   def review_params
-#     params.require(:review).permit(:description)
-#   end
+private
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
+  end
 end
---
+++ spec/features/add_review_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "the add a review process" do
+  it "adds a new review" do
+    product = Product.create(:name => 'Lambas Bread', :cost => '10.00', :origin => 'Lorien')
+    visit product_path(product)
+    click_link 'Write a Review'
+    fill_in 'Author', :with => 'Frodo'
+    fill_in 'Content body', :with => 'This food is OK but could use a little more pizzazz'
+    fill_in 'Rating', :with => 4
+    click_on 'Create Review'
+    expect(page).to have_content 'Frodo'
+  end
+
+  it "gives error when form is not filled out" do
+    product = Product.create(:name => 'Lambas Bread', :cost => '10.00', :origin => 'Lorien')
+    visit new_product_review_path(product)
+    click_on 'Create Review'
+    expect(page).to have_content 'errors'
+  end
+end
